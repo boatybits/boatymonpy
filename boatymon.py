2b4308798580d6f0e01c9c12bc32ffbc35bb39ef
boatybits <bitsboaty@gmail.com>
2020-08-28 09:14:06 +0100

    update

--- boatymon.py
+++ boatymon.py
@@ -16,15 +16,10 @@ class sensors:
         conf = ujson.load(json_data_file)
     
     import utime
-    # ssid1 = "openplotter"
-    # password1 = "12345678"
-    # ssid2 = "padz"
-    # password2 = "12348765"
-    # udpAddr = '10.10.10.1'
-    led = Pin(2, Pin.OUT)           #set internal pin to LED as an output
+    led = Pin(2, Pin.OUT)           #internal LED is on pin 2
 
-    i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000) # set up i2c, pins 21 & 22
-    print('Scan i2c bus...')
+    i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000) 
+    print('Scanning i2c bus...')
     devices = i2c.scan()
     if len(devices) == 0:
         print("No i2c device !")
